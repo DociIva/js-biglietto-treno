7d982a138e765a771ae1af9a58c8f37273e6da8e
DociIva <doci.iva@hotmail.com>
2021-03-19 16:25:26 +0100

    inizio

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere
+ e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, 
+secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo a schermo in forma umana (con massimo due decimali, 
+per indicare centesimi sul prezzo).
+Questo richiederà un minimo di ricerca.
+*/
+
+// CHIEDI ALL'UTENTE I CHILOMETRI CHE VUOLE 
+var kmDaFare = prompt ('Quanti km vuoi percorrere?')
+console.log(kmDaFare);
+
+
+// CHIEDERE QUANTI ANNI HA 
+var età = prompt ('Quanti anni hai ?')
+console.log (età);
